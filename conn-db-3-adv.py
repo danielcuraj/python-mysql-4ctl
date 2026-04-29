15ca9b835c45bfc81b8dd8fbf8212cdbbad84f81
daniel curaj <danielcuraj08@gmail.com>
2026-04-29 21:57:33 +0200

    impostazione

--- conn-db-3-adv.py
+++ conn-db-3-adv.py
@@ -4,9 +4,9 @@ from datetime import date
 # Configurazione della connessione al database
 DB_CONFIG = {
     "host": "127.0.0.1",
-    "user": "NOME_UTENTE_DATABASE",
-    "password": "PASSWORD_DATABASE",
-    "database": "NOME_DATABASE", # lo stesso del NOME_UTENTE
+    "user": "4CTL_curaj.d.231108",
+    "password": "Forzaviola2026!",
+    "database": "4CTL_curaj.d.231108", # lo stesso del NOME_UTENTE
     "port": 3307,
     "cursorclass": pymysql.cursors.Cursor,
     "connect_timeout": 5,
